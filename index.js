d092e73673be2d83434ffbc5541315ae7221d409
brendagouveia <brendagouveia@id.uff.br>
2025-01-16 21:05:50 -0300

    Add two functions, randomNumber1() and imgChange()

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert('Hello World!');
+function randomNumber1() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function imgChange(numb_dice, jogador){
+
+    document.querySelector(".img1").setAttribute("src", "images/dice" + numb_dice + ".png");
+
+}
+//  document.querySelector(".container").textContent = 'Player 1 Wins!'
+
+function main() {
+    const diceNumber = randomNumber1();
+    imgChange(diceNumber, 1);
+   
+  }
+
+main();
